986821db46dae894fdb5ddb23880cbe7bc448381
Yuri <yuricoelho33@gmail.com>
2021-08-14 10:18:32 -0300

    Refatorando os arquivos para utilizar o recurso de layout com blade.

---
+++ resources/views/series/create.blade.php
@@ -0,0 +1,17 @@
+@extends('layout')
+
+@section('cabecalho')
+    Adicionar Séries
+@endsection
+
+@section('conteudo')
+
+    <form method="post">
+        <div class="form-group">
+            <label for="nome">Nome</label>
+            <input type="text" class="form-control" name="nome" id="nome">
+        </div>
+
+        <button class="btn btn-primary mt-2">Adicionar</button>
+    </form>
+@endsection
