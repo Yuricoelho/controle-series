d0bcd82e15bcf9f6fe2b83b4eee3142ae024a972
Yuri <yuricoelho33@gmail.com>
2021-08-16 12:06:44 -0300

    Implementando a funcionalidade de edição do nome das séries.

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,5 @@ Route::get('/series', 'SeriesController@index');
 Route::get('/series/adicionar', 'SeriesController@create');
 Route::post('/series/adicionar', 'SeriesController@store');
 Route::delete('/series/remover/{id}', 'SeriesController@destroy');
+Route::post('/series/{id}/editaNome', 'SeriesController@editaNome');
 Route::get('/series/{serieId}/temporadas', 'TemporadasController@index');
