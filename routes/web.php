259d3bd8f36da2a8d8057fd67050723aed43018f
Yuri <yuricoelho33@gmail.com>
2021-08-16 12:44:21 -0300

    Adicionando a funcionalidade de marcar os episódios como assistidos.

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,4 @@ Route::delete('/series/remover/{id}', 'SeriesController@destroy');
 Route::post('/series/{id}/editaNome', 'SeriesController@editaNome');
 Route::get('/series/{serieId}/temporadas', 'TemporadasController@index');
 Route::get('/temporadas/{temporada}/episodios', 'EpisodiosController@index');
+Route::post('/temporadas/{temporada}/episodios/assistir', 'EpisodiosController@assistir');
